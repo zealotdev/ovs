f96cdad56d0cca50f883a454345dbad159e14da6
geed255 <g33ky.me@pm.me>
2020-06-28 17:26:28 +0300

    Basic Components and Layout styling 💯

---
+++ src/app/components/voteboard/voteboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voteboard',
+  templateUrl: './voteboard.componet.html',
+  styleUrls: ['./voteboard.component.scss'],
+})
+export class VoteboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
