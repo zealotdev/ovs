3600b02c9c139def496e34fc36b98c7eb0fbd42e
geed255 <g33ky.me@pm.me>
2020-07-18 21:51:19 +0300

    _add screen responsiveness support 📱

---
+++ src/app/components/shared/mobile-nav/mobile-nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mobile-nav-app',
+  templateUrl: './mobile-nav.component.html',
+  styleUrls: ['mobile-nav.component.scss'],
+})
+export class MobileNavComponent {}
